339718c15d73a80ef72f3cd08f3d3a798fadb7f1
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-21 18:59:06 -0700

    added HeaderNav

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,15 +1,20 @@
+import HeaderNav from "../HeaderNav/HeaderNav";
+
 const PageHeader = () => {
   return (
     <header>
-      <hgroup>
-        <h3>Already have an Intuit account?</h3>
-        <h3>
-          <a href="https://accounts.intuit.com/signup.html?offering_id=Intuit.ifs.mint&namespace_id=50000026&redirect_url=https%3A%2F%2Fmint.intuit.com%2Foverview.event%3Ftask%3DS#">
-            I forgot my User ID or Password
-          </a>
-        </h3>
-      </hgroup>
-      <button>Sign In</button>
+      <div>
+        <hgroup>
+          <h3>Already have an Intuit account?</h3>
+          <h3>
+            <a href="https://accounts.intuit.com/signup.html?offering_id=Intuit.ifs.mint&namespace_id=50000026&redirect_url=https%3A%2F%2Fmint.intuit.com%2Foverview.event%3Ftask%3DS#">
+              I forgot my User ID or Password
+            </a>
+          </h3>
+        </hgroup>
+        <button>Sign In</button>
+      </div>
+      <HeaderNav />
     </header>
   );
 };
