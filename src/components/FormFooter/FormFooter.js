300c47842e82b401a3938e32910da51efc946e36
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-22 10:10:16 -0700

    restructured elements and styled

--- src/components/FormFooter/FormFooter.js
+++ src/components/FormFooter/FormFooter.js
@@ -1,12 +1,16 @@
+import style from "./FormFooter.module.css";
+
 const FormFooter = () => {
   return (
-    <footer>
-      Invisible reCAPTCHA by Google{" "}
-      <a href="https://www.google.com/intl/en/policies/privacy">
-        Prvivacy Policy
-      </a>{" "}
-      and{" "}
-      <a href="https://www.google.com/intl/en/policies/terms">Terms of Use</a>
+    <footer className={style.formFooter}>
+      <h6>
+        Invisible reCAPTCHA by Google{" "}
+        <a href="https://www.google.com/intl/en/policies/privacy">
+          Prvivacy Policy
+        </a>{" "}
+        and{" "}
+        <a href="https://www.google.com/intl/en/policies/terms">Terms of Use</a>
+      </h6>
     </footer>
   );
 };
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -11,43 +11,45 @@ const SignUpForm = () => {
   return (
     <form className={style.signUpForm}>
       <FormHeader />
-      <div className={style.labeledInput}>
-        <label for="signUpEmail">Email address</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          id="signUpEmail"
-        />
-      </div>
-      <div className={style.labeledInput}>
-        <label for="signUpPhone">Phone (recommended)</label>
-        <input
-          type="phone"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          id="signUpPhone"
-        />
-      </div>
-      <div className={style.labeledInput}>
-        <label for="signUpPass">Password</label>
-        <input
-          type="password"
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-          id="signUpPass"
-        />
-      </div>
-      <div className={style.labeledInput}>
-        <label for="signUpConfirm">Confirm</label>
-        <input
-          type="password"
-          value={confirm}
-          onChange={(e) => setConfirm(e.target.value)}
-          id="signUpConfirm"
-        />
-      </div>
-      <button className={style.signUpBtn}>Create Account</button>
+      <fieldset className={style.signUpFields}>
+        <div className={style.labeledInput}>
+          <label for="signUpEmail">Email address</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="signUpEmail"
+          />
+        </div>
+        <div className={style.labeledInput}>
+          <label for="signUpPhone">Phone (recommended)</label>
+          <input
+            type="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            id="signUpPhone"
+          />
+        </div>
+        <div className={style.labeledInput}>
+          <label for="signUpPass">Password</label>
+          <input
+            type="password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            id="signUpPass"
+          />
+        </div>
+        <div className={style.labeledInput}>
+          <label for="signUpConfirm">Confirm</label>
+          <input
+            type="password"
+            value={confirm}
+            onChange={(e) => setConfirm(e.target.value)}
+            id="signUpConfirm"
+          />
+        </div>
+        <button className={style.signUpBtn}>Create Account</button>
+      </fieldset>
       <FormFooter />
     </form>
   );
