eb1df9a7b26fad3327aa442fc0bf8c967bc05c3e
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-21 18:57:32 -0700

    made HeaderNav

---
+++ src/components/HeaderNav/HeaderNav.js
@@ -0,0 +1,26 @@
+const HeaderNav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <img alt="Intuit Logo" />
+        </li>
+        <li>
+          <ul>
+            <li>
+              <img alt="Mint Logo"></img>
+            </li>
+            <li>
+              <img alt="Quickbooks Logo"></img>
+            </li>
+            <li>
+              <img alt="Turbotax Logo"></img>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default HeaderNav;
