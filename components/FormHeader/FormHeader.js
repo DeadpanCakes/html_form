8a4d6df0f90e961e846401b558bc78640603d32d
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-21 18:39:40 -0700

    made FormHeader

---
+++ components/FormHeader/FormHeader.js
@@ -0,0 +1,15 @@
+const FormHeader = () => {
+  return (
+    <hgroup>
+      <h1>Create an Intuit account</h1>
+      <h3>One account for everything Intuit, including Mint.</h3>
+      <h3>
+        <a href="https://accounts-help.intuit.com/app/intuit/1995107">
+          Learn more
+        </a>
+      </h3>
+    </hgroup>
+  );
+};
+
+export default FormHeader;
