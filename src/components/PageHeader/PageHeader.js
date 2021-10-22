e82d700eb24c47b514dac21549717aa7232624af
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-21 18:51:46 -0700

    made PageHeader

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,17 @@
+const PageHeader = () => {
+  return (
+    <header>
+      <hgroup>
+        <h3>Already have an Intuit account?</h3>
+        <h3>
+          <a href="https://accounts.intuit.com/signup.html?offering_id=Intuit.ifs.mint&namespace_id=50000026&redirect_url=https%3A%2F%2Fmint.intuit.com%2Foverview.event%3Ftask%3DS#">
+            I forgot my User ID or Password
+          </a>
+        </h3>
+      </hgroup>
+      <button></button>
+    </header>
+  );
+};
+
+export default PageHeader;
