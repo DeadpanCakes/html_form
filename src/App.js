51841f7ead9c3aa82fb2c175374f892597da1fae
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-21 18:52:23 -0700

    added PageHeader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import SignUpForm from "./components/SignUpForm/SignUpForm";
 import PageFooter from "./components/PageFooter/PageFooter";
+import PageHeader from "./components/PageHeader/PageHeader";
 
 function App() {
   return (
     <div className="App">
+      <PageHeader />
       <SignUpForm />
       <PageFooter />
     </div>
