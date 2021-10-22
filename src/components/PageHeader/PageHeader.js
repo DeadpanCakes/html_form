f4f1fe0ae5145f88bad8c27d33f1ed15e1b70dab
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-22 10:46:07 -0700

    styled accountCtrls

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,10 +1,11 @@
 import HeaderNav from "../HeaderNav/HeaderNav";
+import style from "./PageHeader.module.css";
 
 const PageHeader = () => {
   return (
-    <header>
-      <div>
-        <hgroup>
+    <header className={style.pageHeader}>
+      <div className={style.accountCtrls}>
+        <hgroup className={style.accountTxt}>
           <h3>Already have an Intuit account?</h3>
           <h3>
             <a href="https://accounts.intuit.com/signup.html?offering_id=Intuit.ifs.mint&namespace_id=50000026&redirect_url=https%3A%2F%2Fmint.intuit.com%2Foverview.event%3Ftask%3DS#">
@@ -12,7 +13,7 @@ const PageHeader = () => {
             </a>
           </h3>
         </hgroup>
-        <button>Sign In</button>
+        <button className={style.signInBtn}>Sign In</button>
       </div>
       <HeaderNav />
     </header>
