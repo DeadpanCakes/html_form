3ed2019c5d3cbf2381d29c7919aa0a4dcfa4b6ff
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-21 18:35:17 -0700

    made FormFooter

---
+++ components/FormFooter/FormFooter.js
@@ -0,0 +1,14 @@
+const FormFooter = () => {
+  return (
+    <footer>
+      Invisible reCAPTCHA by Google{" "}
+      <a href="https://www.google.com/intl/en/policies/privacy">
+        Prvivacy Policy
+      </a>{" "}
+      and{" "}
+      <a href="https://www.google.com/intl/en/policies/terms">Terms of Use</a>
+    </footer>
+  );
+};
+
+export default FormFooter;
